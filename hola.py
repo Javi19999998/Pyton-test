e5a7d06e84f2434b081072664ec1ef7f2ad1a08d
Javi19999998 <javieradcabrera17@gmail.com>
2025-04-15 10:20:58 -0400

    ejemplos de variables e inputs

--- hola.py
+++ hola.py
@@ -1,1 +1,20 @@
-print("Hola mundo")
+# #variables
+# nombre="Javiera"
+# edad= 26
+
+# nombre= input()
+# #ejemplo de concatenacion
+# #print("Hola" , nombre, "y su edad es", edad)
+# print("Hola" , nombre, "y su edad es", edad+5)
+
+# # n1=4
+# # n2=34
+# # print(n1+n2)
+
+# print("Ingrese 2 numeros")
+# n1=int(input())
+# n2=int(input())
+# print("El resultado de la suma es" , n1+n2)
+
+
+
