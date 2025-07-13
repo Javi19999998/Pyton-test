7c519ff2a4e9f5feeb9cde9553ed2a806b559807
Javi19999998 <javieradcabrera17@gmail.com>
2025-07-12 21:37:42 -0400

    INSCRIPCIONES12-07

---
+++ CURSO12-07.py
@@ -0,0 +1,75 @@
+cursos = {
+    "C001": "Programación Básica",
+    "C002": "Introducción a Python",
+    "C003": "Bases de Datos"
+}
+inscripciones = {}
+
+def menu():
+    while True:
+        try:
+            menu=int(input('''
+                        1.- Inscribir Alumno
+                        2.- Consultar Inscripcion
+                        3.- Cancelar Inscripcion
+                        4.- Salir
+                        '''))
+            if menu == 1:
+                inscribir_alumno()
+            elif menu == 2:
+                consultar_inscripcion()
+            elif menu == 3:
+                cancelar_inscripcion()
+            elif menu == 4:
+                print("Saliendo...")
+            else:
+                print("Ingrese opcion del 1 al 4")
+                break
+        except Exception:
+            print("Opcion invalida")
+
+def inscribir_alumno():
+    print("Para inscribirse se necesitan los siguientes datos: ")
+    alumno=input("Ingrese su nombre: ")
+    curso=input("Ingrese codigo del curso: ").upper()
+    while True:
+        codigo = input("Ingrese código de confirmación: ")
+        if validar_codigo(codigo):
+            print("Código válido.")
+            break
+        else:
+            print("Código inválido, inténtalo otra vez.")
+    inscripciones[alumno] = {"curso": curso, "codigo": codigo}
+    print("Inscripción registrada.")
+
+
+def validar_codigo(codigo):
+    largo_ok = len(codigo) >= 6
+    mayus = False
+    digito = False
+    for c in codigo:
+        if c.isupper():
+            mayus = True
+        if c.isdigit():
+            digito = True
+    sin_espacios = " " not in codigo
+    if largo_ok and mayus and digito and sin_espacios:
+        return True
+    else:
+        return False
+
+def consultar_inscripcion():
+    nombre = input("Ingrese el nombre del alumno a consultar")
+    if nombre in inscripciones:
+        print("Curso:", inscripciones[nombre]["curso"])
+        print("Código:", inscripciones[nombre]["codigo"])
+
+def cancelar_inscripcion():
+    nombre = input("Ingrese el nombre del alumno a cancelar: ")
+    if nombre in inscripciones:
+        del inscripciones[nombre]
+        print("Inscripcion eliminada correctamete!")
+    else:
+        print("Ingrese nombre valido")
+
+menu()
